0ecd334ca6e2965ff498301900ac05c623b3b688
SebastianEPH <sebastianeph99@gmail.com>
2020-11-07 10:40:53 -0500

    delete function add

--- BlockWebsites.py
+++ BlockWebsites.py
@@ -1,10 +1,33 @@
-import os
-# Path of the file
+import shutil
+from os import remove
 
 class WebsiteBlock:
     def __init__(self):
         self.hostsPath = r"C:\Windows\System32\drivers\etc\hosts"
-        self.WeblistBlock = r""
+        self.pathListWeb = r""
+        self.hostOriginal = """
+        # Copyright (c) 1993-2009 Microsoft Corp.
+        #
+        # This is a sample HOSTS file used by Microsoft TCP/IP for Windows.
+        #
+        # This file contains the mappings of IP addresses to host names. Each
+        # entry should be kept on an individual line. The IP address should
+        # be placed in the first column followed by the corresponding host name.
+        # The IP address and the host name should be separated by at least one
+        # space.
+        #
+        # Additionally, comments (such as these) may be inserted on individual
+        # lines or following the machine name denoted by a '#' symbol.
+        #
+        # For example:
+        #
+        #      102.54.94.97     rhino.acme.com          # source server
+        #       38.25.63.10     x.acme.com              # x client host
+        
+        # localhost name resolution is handled within DNS itself.
+        #	127.0.0.1       localhost
+        #	::1             localhost
+        """
 
     def __read_file(self, path):
         content = ""
@@ -16,6 +39,19 @@ class WebsiteBlock:
         file = open(path, "a")
         for l in text:
             file.write("\n"+ l)
+    def __delete_file(self,path):
+        try:
+            remove(path)
+        except:
+            pass
+
+    def __backup(self,path1, path2):
+        try:
+            self.__delete_file(path1)
+            shutil.copy(path1, path2)
+            print("Backup - Sucess ")
+        except:
+            print("Backup - there was mistake error ")
 
     def block(self):
 
@@ -26,20 +62,3 @@ class WebsiteBlock:
     def reset(self):
 
         pass
-
-
-
-"""
-
-path = r"Websites List.txt"
-hostPath = r"test.txt"
-
-file = open(path, "r")
-for l in file.readlines():
-    print(l)
-
-# Modifica Host
-
-
-
-"""
