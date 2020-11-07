5286cc9a21eae6c73a6cf20c25a0218749cac0b5
SebastianEPH <sebastianeph99@gmail.com>
2020-11-07 10:05:15 -0500

    read file

--- BlockWebsites.py
+++ BlockWebsites.py
@@ -1,5 +1,34 @@
 import os
 # Path of the file
+
+class WebsiteBlock:
+    def __init__(self):
+        self.hostsPath = r"C:\Windows\System32\drivers\etc\hosts"
+        self.WeblistBlock = r""
+
+    def __read_file(self, path):
+        content = ""
+        file = open(path, "r")
+        for l in file.readlines():
+            content += l
+        return content
+    def __write_file(self,path, text):
+        pass
+
+    def block(self):
+
+        pass
+    def unlock(self):
+
+        pass
+    def reset(self):
+
+        pass
+
+
+
+"""
+
 path = r"Websites List.txt"
 hostPath = r"test.txt"
 
@@ -14,3 +43,4 @@ file = open(hostPath, "a")
 for d in lista:
     file.write("\n"+ d)
 
+"""
